079fb2b5273aae871a813c56eca4d02f491feeaf
Lavantein <nico_afandi_cool@yahoo.co.id>
2016-08-31 00:11:23 +0700

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
           <div class="media-body">
             <h4 class="media-heading"><a href="http://localhost/forumtesting/public/question/{{$post->slug}}">{{ $post->title }}</a></h4>
             <p class="text-right">By : {{ $post->user->name }}</p>
-            <p>{{ $post->body }}</p>
+            <p><?php echo $post->body; ?></p>
             <ul class="list-inline list-unstyled">
               <li><span><i class="glyphicon glyphicon-calendar"></i> {{ $post->created_at->diffForHumans()}}</span></li>
               <li>|</li>
