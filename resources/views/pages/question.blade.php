1e16c89ea50811185b3bff5197a048063455fe8b
Lavantein <nico_afandi_cool@yahoo.co.id>
2016-08-31 00:10:34 +0700

    Update question.blade.php

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -36,5 +36,23 @@
 
 		{!! Form::close() !!}
 
-	</div> <!-- /container -->
+	</div> <!-- /container -->	
+@stop
+
+@section('script')
+	<script type="text/javascript">
+		tinymce.init({
+		    selector: '#body',
+		    theme: 'modern',
+		    plugins: [
+		      'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+		      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+		      'save table contextmenu directionality emoticons template paste textcolor'
+		    ],
+		    content_css: 'css/content.css',
+		    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons | fontsizeselect | fontselect',
+		 	fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+			font_formats: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n'
+	  	});
+	</script>
 @stop
