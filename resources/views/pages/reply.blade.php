e3db91591b353e65ee5d143a4d667f50d74fab83
davin.yu <davin.yu1110@gmail.com>
2016-09-02 08:17:34 +0700

    Change body to tinymce

--- resources/views/pages/reply.blade.php
+++ resources/views/pages/reply.blade.php
@@ -36,7 +36,7 @@
         <div class="media">
           <div class="media-body">
           <p class="text-right">By : {{ $post->user->name }}</p>
-            <p>{{ $reply->body }}</p>
+            <p><?php echo $reply->body; ?></p>
             <ul class="list-inline list-unstyled">
               <li><span><i class="glyphicon glyphicon-calendar"></i> {{ $post->created_at->diffForHumans()}}</span></li>
             </ul>
@@ -56,7 +56,7 @@
       </div>
 
     @empty
-      <p>Be the first to reply</p>
+      <p>Be the first to reply !</p>
     @endforelse
 
     @if(Auth::user())
@@ -77,5 +77,22 @@
     @endif
 
   </div> <!-- /container -->
+
+  @section('script')
+  <script type="text/javascript">
+    tinymce.init({
+        selector: '#body',
+        theme: 'modern',
+        plugins: [
+          'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+          'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+          'save table contextmenu directionality emoticons template paste textcolor'
+        ],
+        content_css: 'css/content.css',
+        toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons | fontsizeselect | fontselect',
+      fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+      font_formats: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n'
+      });
+  </script>
 
 @stop
